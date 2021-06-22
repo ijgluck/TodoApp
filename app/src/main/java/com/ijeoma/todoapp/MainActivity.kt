8505fc35598d21bc8c58cb93ffa5cb6dfdb6a8c5
ijgluck <jgluck54@gmail.com>
2021-06-22 14:27:29 +0100

    second commit

--- app/src/main/java/com/ijeoma/todoapp/MainActivity.kt
+++ app/src/main/java/com/ijeoma/todoapp/MainActivity.kt
@@ -1,23 +1,54 @@
 package com.ijeoma.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.LoginFilter
 import android.widget.Button
 import android.widget.EditText
-import and
-import  android
-import android
+import android.widget.Toast
+
+
 class MainActivity : AppCompatActivity() {
 
-    val username: String="admin"
-    val password:String="admin"
-    private lateinit var usernameInput: EditText
+    val validuserName: String = "admin"
+    val validpassword: String = "admin"
+    private lateinit var userNameInput: EditText
     private lateinit var passwordInput: EditText
-        private lateinit var usernameInput: Button
+    private lateinit var submitButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userNameInput = findViewById(R. id. Username_Input)
+        passwordInput = findViewById(R. id. password_Input)
+        submitButton = findViewById(R. id. submit_Button)
+
+
+        submitButton.setOnClickListener {
+            val userName = userNameInput.text.toString()
+            val password = passwordInput.text.toString()
+            login(userName,password)
+        }
+
+
 
     }
+
+    fun login (userName: String, password: String){
+        if ( isValidCredentials(userName, password)){
+            val intent: Intent = Intent(this, homeActivity :: class.java)
+            intent.putExtra("username", userName)
+            startActivity(intent)
+            }
+            else{
+                Toast.makeText(applicationContext, "Incorrect Credentials", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+private  fun  isValidCredentials (userName: String, password: String): Boolean{
+    if  (userName == validuserName && password == validpassword) return true
+    return false
+}
 }
---
+++ app/src/main/java/com/ijeoma/todoapp/homeActivity.kt
@@ -0,0 +1,21 @@
+package com.ijeoma.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class homeActivity : AppCompatActivity() {
+    private lateinit var usernameDisplay: TextView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        usernameDisplay = findViewById(R.id.usernameDisplay)
+
+        usernameDisplay.text = "Hello Human Being"
+
+    }
+}
